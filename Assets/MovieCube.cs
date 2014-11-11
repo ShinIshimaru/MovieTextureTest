4e7fd61abb091b48de537a0c2f1d0068651b7843
Shin Ishimaru <shinishimaru@gmail.com>
2014-11-11 21:50:42 +0900

    first commit

---
+++ Assets/MovieCube.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovieCube : MonoBehaviour {
+
+	public MovieTexture movTexture;
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.mainTexture = movTexture;
+		movTexture.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!movTexture.isPlaying) {
+			movTexture.Stop ();
+			movTexture.Play ();
+		}
+
+		//renderer.material.mainTexture.Play();
+		float theta = Time.time * Mathf.PI/180*20;
+
+		transform.rotation *= Quaternion.Euler(1, 1, 0);
+
+	}
+}
